7dc66fe5fb89c98d22a3dd21fc6d0234097c93d1
MestafaKamal <em_ben_ramdane@esi.dz>
2020-03-10 00:34:08 +0100

    cleann tsv

--- pre_processing/Python/clean_tsv.py
+++ pre_processing/Python/clean_tsv.py
@@ -3,6 +3,10 @@
 import sys
 import os
 import shutil
+import argparse
+import csv
+
+from os import path
 
 
 def replace(fich):
@@ -14,6 +18,10 @@ def replace(fich):
     data = data.replace("Γ", "Ɣ")
     data = data.replace("ε", "ɛ")
     data = data.replace("Σ", "Ɛ")
+    data = data.replace("ţţ", "tt")
+    data = data.replace("ţ", "tt")
+    data = data.replace("tttt", "tt")
+    data = data.replace("ttt", "tt")
     fin.close()
     print(fich)
     fin = open(fich, "wt")
@@ -22,10 +30,40 @@ def replace(fich):
 
 
 if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser(description="Clean TSV files")
+    PARSER.add_argument("--tsv_dir", help="Directory containing tsv files", type=str)
+    PARAMS = PARSER.parse_args()
+    # ALPHABET = Alphabet(PARAMS.filter_alphabet) if PARAMS.filter_alphabet else None
+
+    if PARAMS.tsv_dir is not None:
+        # for dataset in ["train", "test", "dev", "validated", "other"]:
+        for dataset in ["dev_copy", "validated_copy"]:
+            input_tsv = path.join(path.abspath(PARAMS.tsv_dir), dataset + ".tsv")
+            if os.path.isfile(input_tsv):
+                print("Loading TSV file ", input_tsv)
+                # output_csv = path.join(audio_dir, os.path.split(input_tsv)[-1].replace('tsv', 'csv'))
+                print("Saving new DeepSpeech-formatted CSV file to , output_csv")
+
+                # Get audiofile path and transcript for each sentence in tsv
+                samples = []
+                with open(input_tsv, encoding="utf-8") as input_tsv_file:
+                    reader = csv.DictReader(input_tsv_file, delimiter="\t")
+
+                    i = 0
+                    for row in reader:
+                        sentence = row["sentence"]
+
+                        print("S: ", sentence)
+                        # cleanedSentence = text_cleaning.cleanSentence(sentence)
+                        # print("CS:", cleanedSentence)
+                        i += 1
+                        if i == 100:
+                            break
+"""                            
     try:
         os.mkdir("./ts/")
         print("Directory", str(os.getcwd()) + "/ts created for saving tsv")
     except:
         print("Directory", str(os.getcwd()) + "/tsv already exists!")
     # replace(sys.argv[1])
-
+"""
