f27b87f649730eb3302ba9fcb56e29bce7072170
MestafaKamal <em_ben_ramdane@esi.dz>
2020-06-02 18:26:51 +0100

    build lm

--- kab/a3_language_model.sh
+++ kab/a3_language_model.sh
@@ -13,17 +13,38 @@ echo "Create language model"
 pushd $DATADIR/extracted
 
 if [ ! -f "$DATADIR/lm/lm.binary" ]; then
-		
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Aklanntayri.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amakrad.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amezgun.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amtmeqquntntzeggigin.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Anzaren.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Azalntayri.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Djoher-Amhis-Ouksel-Inig-akked-ccfaya.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Habib-Allah-Mansouri-Tighermin-yemmeccen.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/aberrani.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/abucidan.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/imedyazen.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/isefra.txt -P $DATADIR/extracted/data/cv_kab/
+
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/wiki.kab.txt -P $DATADIR/extracted/data/cv_kab/
+
 		wget https://raw.githubusercontent.com/MestafaKamal/kabyle-language-data/master/tatoebaSentences.txt -P $DATADIR/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/MestafaKamal/kabyle-language-data/master/isefra.txt -P $DATADIR/extracted/data/cv_kab/
-
-		wget https://raw.githubusercontent.com/MestafaKamal/kabyle-language-data/master/imedyazen.txt -P $DATADIR/extracted/data/cv_kab/
-
-		wget https://raw.githubusercontent.com/MestafaKamal/kabyle-language-data/master/wiki.kab.txt -P $DATADIR/extracted/data/cv_kab/
-		
-	
-		cat $DATADIR/extracted/data/cv_kab/wiki.kab.txt $DATADIR/extracted/data/cv_kab/imedyazen.txt $DATADIR/extracted/data/cv_kab/isefra.txt $DATADIR/extracted/data/cv_kab/cvSentences.txt $DATADIR/extracted/data/cv_kab/tatoebaSentences.txt > $DATADIR/extracted/data/cv_kab/allSentences.txt
+		cat $DATADIR/extracted/data/cv_kab/Aklanntayri.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Amakrad.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Amezgun.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Amtmeqquntntzeggigin.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Anzaren.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Azalntayri.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Djoher-Amhis-Ouksel-Inig-akked-ccfaya.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Habib-Allah-Mansouri-Tighermin-yemmeccen.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.bis.txt $DATADIR/extracted/data/cv_kab/aberrani.txt $DATADIR/extracted/data/cv_kab/abucidan.txt $DATADIR/extracted/data/cv_kab/imedyazen.txt $DATADIR/extracted/data/cv_kab/isefra.txt $DATADIR/extracted/data/cv_kab/wiki.kab.txt $DATADIR/extracted/data/cv_kab/tatoebaSentences.txt $DATADIR/extracted/data/cv_kab/cvSentences.txt > $DATADIR/extracted/data/cv_kab/allSentences.txt
 
 		python $HOMEDIR/counter.py $DATADIR/extracted/data/cv_kab/allSentences.txt $DATADIR/extracted/data/cv_kab/top_words.txt 500000
 
