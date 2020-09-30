d7084ceb0275f7f6a3db9206cdc45dd34d571d40
MestafaKamal <em_ben_ramdane@esi.dz>
2020-09-30 16:17:44 +0100

    metadata

---
+++ kab/metadata.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -xe
+
+export METADATA_AUTHOR="DeepSpeech-KAB-Team"
+export METADATA_MODEL_NAME="deepspeech-kab"
+export METADATA_MODEL_VERSION="1.2"
+export METADATA_CONTACT_INFO="https://discourse.mozilla.org"
+export METADATA_LICENSE="MIT-0"
+export METADATA_LANGUAGE="kab-KAB"
+export METADATA_MIN_DS_VERSION="0.7"
+export METADATA_MAX_DS_VERSION="0.9"
+export METADATA_DESCRIPTION="A free and re-usable Kabyle model for DeepSpeech"
