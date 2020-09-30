b679b97ddfcbc02a2605ad822d7372a464aff587
MestafaKamal <em_ben_ramdane@esi.dz>
2020-09-30 16:18:55 +0100

    build LM

--- kab/a3_language_model.sh
+++ kab/a3_language_model.sh
@@ -10,66 +10,93 @@ set -xe
 
 echo "Create language model"
 
-pushd $DATADIR/extracted
+if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+	OLD_LANG=${LANG}
+	export LANG=${LM_ICONV_LOCALE}
+fi;
 
-if [ ! -f "$DATADIR/lm/lm.binary" ]; then
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Aklanntayri.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+pushd /mnt/extracted
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amakrad.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+if [ ! -f "/mnt/lm/lm.binary" ]; then
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amezgun.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Aklanntayri.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amtmeqquntntzeggigin.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amakrad.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Anzaren.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amezgun.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Azalntayri.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Amtmeqquntntzeggigin.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Djoher-Amhis-Ouksel-Inig-akked-ccfaya.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Anzaren.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Habib-Allah-Mansouri-Tighermin-yemmeccen.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Azalntayri.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.bis.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Djoher-Amhis-Ouksel-Inig-akked-ccfaya.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/aberrani.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Habib-Allah-Mansouri-Tighermin-yemmeccen.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/abucidan.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.bis.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/imedyazen.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/aberrani.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/isefra.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/abucidan.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/wiki.kab.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/imedyazen.txt -P /mnt/extracted/data/cv_kab/
 
-		wget https://raw.githubusercontent.com/MestafaKamal/kabyle-language-data/master/tatoebaSentences.txt -P $DATADIR/extracted/data/cv_kab/
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/isefra.txt -P /mnt/extracted/data/cv_kab/
 
-		cat $DATADIR/extracted/data/cv_kab/Aklanntayri.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Amakrad.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Amezgun.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Amtmeqquntntzeggigin.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Anzaren.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Azalntayri.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Djoher-Amhis-Ouksel-Inig-akked-ccfaya.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Habib-Allah-Mansouri-Tighermin-yemmeccen.pdf.bis.txt $DATADIR/extracted/data/cv_kab/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.bis.txt $DATADIR/extracted/data/cv_kab/aberrani.txt $DATADIR/extracted/data/cv_kab/abucidan.txt $DATADIR/extracted/data/cv_kab/imedyazen.txt $DATADIR/extracted/data/cv_kab/isefra.txt $DATADIR/extracted/data/cv_kab/wiki.kab.txt $DATADIR/extracted/data/cv_kab/tatoebaSentences.txt $DATADIR/extracted/data/cv_kab/cvSentences.txt > $DATADIR/extracted/data/cv_kab/allSentences.txt
+		wget https://raw.githubusercontent.com/mozillakab/Kabyletexts/master/lm/wiki.kab.txt -P /mnt/extracted/data/cv_kab/
 
-		python $HOMEDIR/counter.py $DATADIR/extracted/data/cv_kab/allSentences.txt $DATADIR/extracted/data/cv_kab/top_words.txt 500000
+		wget https://raw.githubusercontent.com/MestafaKamal/kabyle-language-data/master/tatoebaSentences.txt -P /mnt/extracted/data/cv_kab/
 
-		lmplz	--order 4 \
-			--temp_prefix $DATADIR/tmp/ \
-			--memory 80% \
-			--text $DATADIR/extracted/data/cv_kab/allSentences.txt \
-			--arpa $DATADIR/lm/kab_words.arpa \
-			--skip_symbols \
-			--prune 0 0 1
+		cat /mnt/extracted/data/cv_kab/Aklanntayri.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Amakrad.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Amezgun.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Amtmeqquntntzeggigin.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Anzaren.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Azalntayri.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Djoher-Amhis-Ouksel-Inig-akked-ccfaya.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Habib-Allah-Mansouri-Tighermin-yemmeccen.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/Trad-n-Yugurten-Traduc-M.O-Kheddam.pdf.bis.txt \
+		/mnt/extracted/data/cv_kab/aberrani.txt \
+		/mnt/extracted/data/cv_kab/abucidan.txt \
+		/mnt/extracted/data/cv_kab/imedyazen.txt \
+		/mnt/extracted/data/cv_kab/isefra.txt \
+		/mnt/extracted/data/cv_kab/wiki.kab.txt \
+		/mnt/extracted/data/cv_kab/tatoebaSentences.txt \
+		/mnt/extracted/data/cv_kab/cvSentences.txt > /mnt/extracted/data/cv_kab/allSentences.txt
 
-		filter single model:$DATADIR/lm/kab_words.arpa $DATADIR/lm/lm_kab_filtered.arpa < $DATADIR/extracted/data/cv_kab/top_words.txt
 
-		build_binary -a 255 \
-			-q 8 \
-			trie \
-			-T \
-			-s \
-			$DATADIR/lm/lm_kab_filtered.arpa \
-			$DATADIR/lm/lm.binary		
+
+
+pushd $HOME/ds/
+
+	if [ ! -f "/mnt/lm/lm.binary" ]; then
+			python data/lm/generate_lm.py \
+				--input_txt /mnt/extracted/data/cv_kab/allSentences.txt \
+				--output_dir /mnt/lm/ \
+				--top_k ${LM_TOP_K} \
+				--kenlm_bins $HOME/kenlm/build/bin/ \
+				--arpa_order 4 \
+				--max_arpa_memory "85%" \
+				--arpa_prune "0|0|1" \
+				--binary_a_bits 255 \
+				--binary_q_bits 8 \
+				--binary_type trie
 	fi;
-    if [ ! -f "$DATADIR/lm/trie" ]; then	
-		$DS_DIR/native_client/generate_trie $HOMEDIR/${MODEL_LANGUAGE}/data_kab/alphabet.txt $DATADIR/lm/lm.binary $DATADIR/lm/trie
-	fi;
 
+	./generate_scorer_package \
+		--alphabet $HOMEDIR/${MODEL_LANGUAGE}/data_kab/alphabet.txt \
+		--lm /mnt/lm/lm.binary \
+		--vocab /mnt/lm/vocab-${LM_TOP_K}.txt \
+		--package /mnt/lm/kenlm.scorer \
+		--default_alpha ${LM_ALPHA} \
+		--default_beta ${LM_BETA}
 
 popd
 
+
+if [ "${ENGLISH_COMPATIBLE}" = "1" ]; then
+	export LANG=${OLD_LANG}
+fi;
